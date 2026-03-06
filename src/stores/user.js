3a2bc1b12f2acd63705accf74ced89334f8d6853
pa <maplenagisa@gmail.com>
2026-03-06 18:14:24 +0900

    add @tanstack/query

--- src/stores/user.js
+++ src/stores/user.js
@@ -29,6 +29,7 @@ import {
     instanceRequest,
     userRequest
 } from '../api';
+import { patchUserFromEvent } from '../query';
 import { processBulk, request } from '../service/request';
 import { AppDebug } from '../service/appConfig';
 import { database } from '../service/database';
@@ -765,6 +766,7 @@ export const useUserStore = defineStore('User', () => {
                 }
             }
         }
+        patchUserFromEvent(ref);
         return ref;
     }
 
@@ -920,9 +922,6 @@ export const useUserStore = defineStore('User', () => {
                     let inCurrentWorld = false;
                     if (locationStore.lastLocation.playerList.has(D.ref.id)) {
                         inCurrentWorld = true;
-                    }
-                    if (args.cache) {
-                        userRequest.getUser(args.params);
                     }
                     if (userId !== currentUser.value.id) {
                         database
