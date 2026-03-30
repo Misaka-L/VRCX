4bdf1c674e042500fb1076b40cd5a5396334536b
pa <maplenagisa@gmail.com>
2026-03-30 21:01:24 +0900

    fix: split i18n when build and npm audit fix

--- src/vite.config.js
+++ src/vite.config.js
@@ -64,7 +64,8 @@ function getAssetLanguage(assetId) {
  * @param moduleId
  */
 function getManualChunk(moduleId) {
-    const language = getAssetLanguage(moduleId);
+    const basename = moduleId.split('/').pop();
+    const language = getAssetLanguage(basename);
     if (!language) return;
 
     return `i18n/${language}`;
@@ -145,7 +146,7 @@ export default defineConfig(({ mode }) => {
                     customMedia: true
                 },
                 errorRecovery: true,
-                targets: browserslistToTargets(browserslist('Chrome 144'))
+                targets: browserslistToTargets(browserslist('Chrome 145'))
             }
         },
         optimizeDeps: {
@@ -174,8 +175,7 @@ export default defineConfig(({ mode }) => {
             strictPort: true
         },
         build: {
-            target: 'chrome144',
-            cssTarget: 'chrome144',
+            target: 'chrome145',
             outDir: '../build/html',
             license: true,
             emptyOutDir: true,
@@ -185,6 +185,7 @@ export default defineConfig(({ mode }) => {
             sourcemap: buildAndUploadSourceMaps ? 'hidden' : false,
             assetsInlineLimit(filePath) {
                 if (isFont(filePath)) return 0;
+                if (filePath.endsWith('.json')) return 0;
                 return 40960;
             },
             rolldownOptions: {
